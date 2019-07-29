febfe0767c0f69d65ff7ed097d1263c408133fc9
折影轻梦 <776194970@qq.com>
2019-07-29 21:08:14 +0800

    🐛 修复一定情况下图片消失的问题

--- source/js/app.js
+++ source/js/app.js
@@ -1,14 +1,16 @@
 $("table:has(img)").addClass("nexmoe-album");
-$("article img").each(function () {
-     var element = document.createElement("a");
-     $(element).attr("data-fancybox", "gallery");
-     $(element).attr("href", $(this).attr("src"));
-     $(this).wrap(element);
-});
 
 $("#nexmoe-content img").each(function () {
     $(this).attr("data-original", $(this).attr("src"));
+    $(this).attr("src", "");
     $(this).attr("referrerPolicy", "no-referrer");
+});
+
+$("article img").each(function () {
+    var element = document.createElement("a");
+    $(element).attr("data-fancybox", "gallery");
+    $(element).attr("href", $(this).attr("src"));
+    $(this).wrap(element);
 });
 
 $("#nexmoe-sidebar a").addClass("mdui-ripple");
