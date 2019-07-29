d7cfa91afe756de86efc93cb49ba7775aa68f473
折影轻梦 <776194970@qq.com>
2019-07-29 19:21:42 +0800

    🐛 修复一定情况下图片消失的问题

--- source/js/app.js
+++ source/js/app.js
@@ -3,14 +3,16 @@ $("article img").each(function () {
      var element = document.createElement("a");
      $(element).attr("data-fancybox", "gallery");
      $(element).attr("href", $(this).attr("src"));
-     $(this).attr("data-original", $(this).attr("src"));
-     $(this).attr("src", "");
      $(this).wrap(element);
- });
- $("#nexmoe-content img").each(function () {
+});
+
+$("#nexmoe-content img").each(function () {
     $(this).attr("data-original", $(this).attr("src"));
     $(this).attr("src", "");
+    $(this).attr("referrerPolicy", "no-referrer");
 });
- $("#nexmoe-sidebar a").addClass("mdui-ripple");
- mdui.mutation();
+
+$("#nexmoe-sidebar a").addClass("mdui-ripple");
+mdui.mutation();
+
 $("img").lazyload({effect: "fadeIn"});
