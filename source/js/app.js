0018c771b26ae865d0157c22f236203f1a2a2e03
suikastar <suikatomoki@gmail.com>
2019-07-30 14:44:22 +0800

    Revert "❌ 去除灯箱"
    This reverts commit 72d1429f891a76d94800341b9304721716b68d80.
    
    	修改：     layout/_partial/after-footer.ejs
    	修改：     layout/layout.ejs
    	修改：     source/css/style.styl
    	修改：     source/js/app.js

--- source/js/app.js
+++ source/js/app.js
@@ -2,25 +2,21 @@ $("table")
   .has("img")
   .addClass("nexmoe-album");
 
-$("img").each(function() {
+$("img").each(function () {
   $(this).attr("data-src", $(this).attr("src"));
   $(this).attr("src", "");
   $(this).addClass("lazyload");
   $(this).attr("referrerPolicy", "no-referrer");
 });
 
-/* $("article img").each(function() {
+$("article img").each(function () {
   var element = document.createElement("a");
-  $(this)
-    .parent()
-    .attr("data-src", $(this).attr("data-src"));
   $(element).attr("href", $(this).attr("data-src"));
   var img = $(this)
     .parent()
     .html();
   $(this).replaceWith($(element).html(img));
 });
-*/
 
 $("#nexmoe-sidebar a").addClass("mdui-ripple");
 mdui.mutation();
