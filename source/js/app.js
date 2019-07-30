b22d20e5146112ef0ff00cf3f914c420b91cb1d6
折影轻梦 <776194970@qq.com>
2019-07-30 12:56:19 +0800

    🌩 灯箱问题未解决

--- source/js/app.js
+++ source/js/app.js
@@ -1,17 +1,21 @@
-$("table").has('img').addClass("nexmoe-album");
+$("table")
+  .has("img")
+  .addClass("nexmoe-album");
 
 $("img").each(function () {
-    $(this).attr("data-src", $(this).attr("src"));
-    $(this).attr("src", "");
-    $(this).addClass('lazyload')
-    $(this).attr("referrerPolicy", "no-referrer");
+  $(this).attr("data-src", $(this).attr("src"));
+  $(this).attr("src", "");
+  $(this).addClass("lazyload");
+  $(this).attr("referrerPolicy", "no-referrer");
 });
 
 $("article img").each(function () {
-    var element = document.createElement("a");
-    $(element).attr("href", $(this).attr("data-src"));
-    var img = $(this).parent().html();
-    $(this).replaceWith($(element).html(img));
+  var element = document.createElement("a");
+  $(element).attr("href", $(this).attr("data-src"));
+  var img = $(this)
+    .parent()
+    .html();
+  $(this).replaceWith($(element).html(img));
 });
 
 $("#nexmoe-sidebar a").addClass("mdui-ripple");
