5c4b0b441ed8b8e63ad2f92d50e5d889e43f02fc
折影轻梦 <776194970@qq.com>
2019-07-31 14:43:56 +0800

    🐛 修复了些不可描述的问题

--- source/js/app.js
+++ source/js/app.js
@@ -9,5 +9,13 @@ $("#nexmoe-content img").each(function() {
   $(this).attr("referrerPolicy", "no-referrer");
 });
 
+$("article:not(.nexmoe-py) img").each(function() {
+  var element = document.createElement("a");
+  $(element).attr("data-fancybox", "gallery");
+  $(element).attr("href", $(this).attr("data-src"));
+  $(element).attr("title", $(this).attr("alt"));
+  $(this).wrap(element);
+});
+
 $("#nexmoe-sidebar a").addClass("mdui-ripple");
 mdui.mutation();
