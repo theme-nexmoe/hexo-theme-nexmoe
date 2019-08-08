603d9eb28b21049132835d91eecac306fd40b290
Xtao_dada <1344089750@qq.com>
2019-08-08 21:06:30 +0800

    修改错误

--- WIKI/.vuepress/config.js
+++ WIKI/.vuepress/config.js
@@ -108,6 +108,7 @@ module.exports = {
                 sidebarDepth: 2,
                 children: [
                     'basic/icon',
+	            'basic/color',
                 ],
             },
         ],
