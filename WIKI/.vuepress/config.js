0a5cb1d0884d81700aad03b370548abbeb623aa7
折影轻梦 <776194970@qq.com>
2019-08-08 17:24:02 +0800

    🎉 文档使用 vuepress

---
+++ WIKI/.vuepress/config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  title: 'Nexmoe 主题文档',
+  description: 'Nexmoe 主题文档',
+  base: '/',
+  head: [
+    ['link', {
+      rel: 'manifest ',
+      href: '/manifest.json'
+    }],
+  ],
+  plugins: [
+    '@vuepress/pwa', {
+      serviceWorker: true,
+    }
+  ],
+  themeConfig: {
+    lastUpdated: '最后编辑',
+
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'nexmoe/hexo-theme-nexmoe',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'GitHub',
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: 'nexmoe/hexo-theme-nexmoe',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'WIKI',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面',
+    nav: [{
+      text: 'Hexo',
+      link: '/hexo/'
+    }, ],
+    sidebar: 'auto',
+    sidebar: {
+      '/senior/': [{
+          title: '必修二',
+          collapsable: false,
+          children: [
+            '/senior/compulsory2/氓',
+            '/senior/compulsory2/采薇',
+            '/senior/compulsory2/离骚',
+          ]
+        },
+        {
+          title: '必修三',
+          collapsable: false,
+        },
+        {
+          title: '必修五',
+          collapsable: false,
+          children: [
+            '/senior/compulsory5/归去来兮辞·并序',
+            '/senior/compulsory5/滕王阁序',
+            '/senior/compulsory5/逍遥游',
+            '/senior/compulsory5/陈情表',
+          ]
+        }
+      ],
+    },
+  },
+}
---
+++ WIKI/README.md
@@ -0,0 +1,51 @@
+---
+home: true
+heroImage: https://i.loli.net/2019/08/08/5ceZJrGxLk8Shug.png
+heroText: Nexmoe
+tagline: 🔥 一个比较特别的 Hexo 主题
+actionText: 快速上手 →
+actionLink: /hexo/zh/start.html
+features:
+- title: 简洁至上
+  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+footer: Made with ❤ by Nexmoe
+---
+
+<div class="item-list">
+    <router-link :to="page.path" v-for="page of $site.pages.filter(item => item.path !== '/')" :key="page.key" class="item">
+        {{page.title}}
+        <br>
+        ---
+    </router-link>
+</div>
+
+<style>
+    .item-list {
+        margin: -5px;
+        margin-bottom: 10px;
+    }
+    .item-list::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+    .item-list .item {
+        float: left;
+        width: calc(25% - 10px);
+        margin: 5px;
+        background-color: #3eaf7c;
+        color: #fff;
+        border-radius: 6px;
+        padding: 12px 16px;
+        box-sizing: border-box;
+    }
+    @media screen and (max-width:768px) {
+        .item-list .item {
+            width: calc(50% - 10px);
+        }
+    }
+</style>
