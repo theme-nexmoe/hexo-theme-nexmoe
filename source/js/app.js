847dd3d375f9e8b735bc80d5da5bad0759ebd9d9
折影轻梦 <776194970@qq.com>
2019-08-12 14:56:30 +0800

    🎨 beautify

--- source/js/app.js
+++ source/js/app.js
@@ -1,31 +1,31 @@
 const getRealPath = (pathname, desc = false) => {
   if (!pathname) {
-      pathname = window.location.pathname
+    pathname = window.location.pathname;
   }
-  let names = pathname.split("/")
+  let names = pathname.split("/");
   if (desc === false) {
-      for (let i = names.length - 1; i >= 0; --i) {
-          let name = names[i].trim()
-          if (name.length > 0 && name !== "/" && name !== "index.html") {
-              return name
-          }
+    for (let i = names.length - 1; i >= 0; --i) {
+      let name = names[i].trim();
+      if (name.length > 0 && name !== "/" && name !== "index.html") {
+        return name;
       }
+    }
   } else {
-      for (let i = 0; i < names.length; ++i) {
-          let name = names[i].trim()
-          if (name.length > 0 && name !== "/" && name !== "index.html") {
-              return name
-          }
+    for (let i = 0; i < names.length; ++i) {
+      let name = names[i].trim();
+      if (name.length > 0 && name !== "/" && name !== "index.html") {
+        return name;
       }
+    }
   }
-  return "/"
-}
-let links = document.querySelectorAll('.nexmoe-list-item');
+  return "/";
+};
+let links = document.querySelectorAll(".nexmoe-list-item");
 let rootRealPath = getRealPath(window.location.pathname, true);
 for (let link of links) {
   let linkPath = link.getAttribute("href");
   if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
-      link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
+    link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
   }
 }
 $("table")
