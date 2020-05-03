fd6255dc3aa4c26deca74c9136d7cd5cef71cb69
折影轻梦 <776194970@qq.com>
2020-05-03 15:53:59 +0800

    删除了 InstantClick 浪费了点生命

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,40 @@
+const getRealPath = (pathname, desc = false) => {
+  if (!pathname) {
+    pathname = window.location.pathname;
+  }
+  let names = pathname.split("/");
+  if (desc === false) {
+    for (let i = names.length - 1; i >= 0; --i) {
+      let name = names[i].trim();
+      if (name.length > 0 && name !== "/" && name !== "index.html") {
+        return name;
+      }
+    }
+  } else {
+    for (let i = 0; i < names.length; ++i) {
+      let name = names[i].trim();
+      if (name.length > 0 && name !== "/" && name !== "index.html") {
+        return name;
+      }
+    }
+  }
+  return "/";
+};
+let links = document.querySelectorAll(".nexmoe-list-item");
+let rootRealPath = getRealPath(window.location.pathname, true);
+for (let link of links) {
+  let linkPath = link.getAttribute("href");
+  if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
+    link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
+  }
+}
+
 var $ = mdui.JQ;
+$("table")
+  .has("img")
+  .addClass("nexmoe-album");
 
 function search() {
   window.open($("#search_form").attr("action_e") + " " + $("#search_value").val());
   return false;
 }
-$("table")
-.has("img")
-.addClass("nexmoe-album");
