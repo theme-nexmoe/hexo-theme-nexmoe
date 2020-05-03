3c5f1f7d9575a5584d5cd939a1e96e7dfa86ccf6
折影轻梦 <776194970@qq.com>
2020-05-03 15:41:37 +0800

    2333

--- source/js/app.js
+++ source/js/app.js
@@ -4,19 +4,6 @@ function search() {
   window.open($("#search_form").attr("action_e") + " " + $("#search_value").val());
   return false;
 }
-
-function reLoad() {
-  loadMeting();
-  $("table")
-    .has("img")
-    .addClass("nexmoe-album");
-  mdui.mutation();
-  document.querySelectorAll('pre code').forEach((block) => {
-    hljs.highlightBlock(block);
-  });
-}
-
-reLoad();
-InstantClick.on('change', function() {
-  reLoad();
-});
+$("table")
+.has("img")
+.addClass("nexmoe-album");
