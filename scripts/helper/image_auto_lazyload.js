958eb37176bf59300a770bbf0384abb5e77fee31
折影轻梦 <776194970@qq.com>
2021-01-11 21:33:18 +0800

    新增图虫和站酷图标

--- scripts/helper/image_auto_lazyload.js
+++ scripts/helper/image_auto_lazyload.js
@@ -1,6 +1,18 @@
-'use strict';
+"use strict";
 
 function imageAutoLazyloadHelper(content) {
-    return content.replace(/<img.*?src="(.*?)" alt="(.*?)".*?\/?>/gi, '<img data-fancybox="gallery" data-sizes="auto" data-src="$1" alt="$2" class="lazyload">');
+	var str = content.replace(
+		/<img.*?src="(.*?)" alt="(.*?)".*?\/?>/gi,
+		'<img data-fancybox="gallery" data-sizes="auto" data-src="$1" alt="$2" class="lazyload">'
+	);
+	str = str.replace(
+        /src="images\//g,
+		'src="https://cdn.jsdelivr.net/gh/nexmoe/nexmoe.github.io@latest/images/'
+	);
+	str = str.replace(
+        /\.\.\/\.\.\/images\//g,
+		'https://cdn.jsdelivr.net/gh/nexmoe/nexmoe.github.io@latest/images/'
+	);
+	return str;
 }
-hexo.extend.helper.register('image_auto_lazyload', imageAutoLazyloadHelper);
+hexo.extend.helper.register("image_auto_lazyload", imageAutoLazyloadHelper);
