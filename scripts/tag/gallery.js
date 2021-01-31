7936fab255004e0dd93efb418c29dcc6b39b0d07
折影轻梦 <776194970@qq.com>
2021-01-31 18:29:35 +0800

    相片集 Pro

---
+++ scripts/tag/gallery.js
@@ -0,0 +1,21 @@
+"use strict";
+
+hexo.extend.tag.register(
+	"gallery",
+	function (args, content) {
+		return `<div class="justified-gallery">${hexo.render
+			.renderSync({
+				text: content,
+				engine: "markdown",
+			})
+			.replace(/<br>/gi, "")
+			.replace(/<p>/gi, "")
+			.replace(/<p\/?>/gi, "")
+			.replace(
+				/<img.*?src="(.*?)" alt="(.*?)".*?\/?>/gi,
+				'<div><img alt="$2" src="$1" data-src="$1" data-fancybox="gallery"></div>'
+			)}
+            </div>`;
+	},
+	{ ends: true }
+);
