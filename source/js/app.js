d7a4d91c6a9f1e2a6cfbcc94661b0e9839d5fa61
折影轻梦 <nexmoe@qq.com>
2021-03-01 13:42:19 +0800

    平滑跳转同时修复锚点链接被转义

--- source/js/app.js
+++ source/js/app.js
@@ -29,11 +29,28 @@ for (let link of links) {
   }
 }
 
+// 表格相册
 $("table")
   .has("img")
   .addClass("nexmoe-album");
 
+// 搜索
 function search() {
   window.open($("#search_form").attr("action_e") + " " + $("#search_value").val());
   return false;
 }
+
+// 平滑跳转同时修复锚点链接被转义
+$(document).ready(function () {
+
+  $("a.toc-link").click(function (ev) {
+    ev.preventDefault();
+    $("html, body").animate({
+      scrollTop: $(decodeURI($(this).attr("href"))).offset().top - 25
+    }, {
+      duration: 500,
+      easing: "swing"
+    });
+  });
+  return false;
+});
