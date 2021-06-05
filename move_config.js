412c14ea3db8f64d7dc5830c21adb6bde08cee9d
折影轻梦 <nexmoe@qq.com>
2021-06-05 15:22:41 +0800

    Update move_config.js

--- move_config.js
+++ move_config.js
@@ -4,8 +4,9 @@ if (fs.existsSync('../hexo/package.json')){
   const version = JSON.parse(fs.readFileSync('../hexo/package.json')).version
   if (version.split('.')[0] === '5') {
     const oldConfigPath = '../../_config.old.yml'
-    const configPath = '../../_config.webstack.yml'
+    const configPath = '../../_config.nexmoe.yml'
     if (!fs.existsSync(configPath) && !fs.existsSync(oldConfigPath)) {
+      fs.writeFileSync(oldConfigPath, fs.readFileSync(configPath))
       fs.writeFileSync(configPath, fs.readFileSync('./_config.yml'))
     }
     fs.unlinkSync('./_config.yml')
