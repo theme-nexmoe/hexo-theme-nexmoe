81648a2a8fb4aba5c79cf391227516c2275c98c6
折影轻梦 <nexmoe@qq.com>
2021-07-25 10:33:32 +0800

    更好的方式实现导航栏高亮

--- source/js/app.js
+++ source/js/app.js
@@ -1,34 +1,3 @@
-const getRealPath = (pathname, desc = false) => {
-  if (!pathname) {
-    pathname = window.location.pathname;
-  }
-  let names = pathname.split("/");
-  if (desc === false) {
-    for (let i = names.length - 1; i >= 0; --i) {
-      let name = names[i].trim();
-      if (name.length > 0 && name !== "/" && name !== "index.html") {
-        return name;
-      }
-    }
-  } else {
-    for (let i = 0; i < names.length; ++i) {
-      let name = names[i].trim();
-      if (name.length > 0 && name !== "/" && name !== "index.html") {
-        return name;
-      }
-    }
-  }
-  return "/";
-};
-let links = document.querySelectorAll(".nexmoe-list-item");
-let rootRealPath = getRealPath(window.location.pathname, true);
-for (let link of links) {
-  let linkPath = link.getAttribute("href");
-  if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
-    link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
-  }
-}
-
 // 表格相册
 $("table")
   .has("img")
@@ -52,12 +21,6 @@ $(document).ready(function () {
     });
   });
 });
-
-function catalogue() {
-  var inst = new mdui.Drawer('#drawer');
-  inst.toggle();
-  $("body").toggleClass("catalogue");
-}
 
 $(document).on("copy", function(){
   if(!window.copyTip){ return; }
