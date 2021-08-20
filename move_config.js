2e68fe006e3bbfafb94e32fc7b91b213fbf666d9
Thin Buffalo <mcluozc@163.com>
2021-08-20 12:16:08 +0800

    移动配置文件

--- move_config.js
+++ move_config.js
@@ -5,15 +5,14 @@ if (fs.existsSync('../hexo/package.json')){
   if (version.split('.')[0] === '5') {
     const oldConfigPath = '../../_config.old.yml'
     const configPath = '../../_config.nexmoe.yml'
-    if (!fs.existsSync(configPath) && !fs.existsSync(oldConfigPath)) {
-      fs.writeFileSync(configPath, fs.readFileSync('./_config.yml'))
-      console.log('Successful installation!');
-      console.log('Please modify the file `_config.nexmoe.yml` in the root directory');
+    if (fs.existsSync(configPath)) {
+      fs.writeFileSync( '../../_config.new.yml', fs.readFileSync('./source/_config.yml'))
+    } else if(!fs.existsSync(oldConfigPath)) {
+      fs.writeFileSync(configPath, fs.readFileSync('./source/_config.yml'))
+      console.log('Successful installation!')
+      console.log('Please modify the file `_config.nexmoe.yml` in the root directory')
     }
-    if (fs.existsSync(configPath)){
-      fs.writeFileSync( '../../_config.new.yml', fs.readFileSync('./_config.yml'))
-    }
-    fs.unlinkSync('./_config.yml')
+    fs.unlinkSync('./source/_config.yml')
   }
 }
 
