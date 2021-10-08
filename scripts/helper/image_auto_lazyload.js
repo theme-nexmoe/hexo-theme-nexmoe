cb655842726c32da5b3e974fb3152465442a345d
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-10-08 12:13:44 +0800

    Update image_auto_lazyload.js

--- scripts/helper/image_auto_lazyload.js
+++ scripts/helper/image_auto_lazyload.js
@@ -5,14 +5,6 @@ function imageAutoLazyloadHelper(content) {
 		/<img.*?src="(.*?)" alt="(.*?)".*?\/?>/gi,
 		'<img data-fancybox="gallery" data-sizes="auto" data-src="$1" alt="$2" class="lazyload">'
 	);
-	str = str.replace(
-        /src="images\//g,
-		'src="https://cdn.jsdelivr.net/gh/nexmoe/nexmoe.github.io@latest/images/'
-	);
-	str = str.replace(
-        /\.\.\/\.\.\/images\//g,
-		'https://cdn.jsdelivr.net/gh/nexmoe/nexmoe.github.io@latest/images/'
-	);
 	return str;
 }
 hexo.extend.helper.register("image_auto_lazyload", imageAutoLazyloadHelper);
