960d8704af08e94ecf09c74cf319769e2fdf1e90
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2022-09-30 14:29:54 +0800

    feat: image on error & auto height of cover #226

--- scripts/helper/image_filter.js
+++ scripts/helper/image_filter.js
@@ -5,7 +5,7 @@ hexo.extend.filter.register("after_post_render", function (data) {
 
 	data.content = data.content.replace(
 		/<img.*?src="(.*?)" alt="(.*?)".*?\/?>/gi,
-		'<img onerror="imgerrorfun();" data-fancybox="gallery" src="$1" alt="$2" data-caption="$2" loading="lazy">'
+		'<img onerror="imgOnError(this);" data-fancybox="gallery" src="$1" alt="$2" data-caption="$2" loading="lazy">'
 	);
 
 	if (themeCfg.imageCDN.enable) {
