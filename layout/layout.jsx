a7ca8aa73cf3795ecc3995a584095c64aebc8cca
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2023-03-18 22:55:19 +0800

    chore: jsx

---
+++ layout/layout.jsx
@@ -0,0 +1,49 @@
+const { Component } = require("inferno");
+const Background = require("./_partial/background");
+
+module.exports = class extends Component {
+	render() {
+		const { config, page, theme, partial, url_for } = this.props;
+
+		const language = page.lang || page.language || config.language;
+
+		return (
+			<html lang={language ? language.substr(0, 2) : ""}>
+				<head
+					dangerouslySetInnerHTML={{
+						__html: partial("_partial/head"),
+					}}
+				></head>
+				<body class="mdui-drawer-body-left">
+					<Background theme={theme} config={config} url_for={url_for} />
+					<div
+						id="nexmoe-header"
+						dangerouslySetInnerHTML={{
+							__html: partial("_layout/single/header"),
+						}}
+					></div>
+					<div
+						id="nexmoe-content"
+						dangerouslySetInnerHTML={{
+							__html:
+								partial("_layout/single/content") +
+								partial("_partial/right"),
+						}}
+					></div>
+
+					<div
+						dangerouslySetInnerHTML={{
+							__html: partial("_partial/searchbox"),
+						}}
+					></div>
+
+					<div
+						dangerouslySetInnerHTML={{
+							__html: theme.slotFooter,
+						}}
+					></div>
+				</body>
+			</html>
+		);
+	}
+};
