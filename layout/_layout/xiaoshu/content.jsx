9a2422507970c23a2b6690c1151e4236ec30edaa
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2023-03-19 15:39:14 +0800

    feat: xiaoshu init

---
+++ layout/_layout/xiaoshu/content.jsx
@@ -0,0 +1,14 @@
+const { Component } = require("inferno");
+
+module.exports = class extends Component {
+	render() {
+		const { body } = this.props;
+
+		return (
+			<div
+				class="nexmoe-container"
+				dangerouslySetInnerHTML={{ __html: body }}
+			></div>
+		);
+	}
+};
--- layout/layout.jsx
+++ layout/layout.jsx
@@ -4,8 +4,11 @@ const Background = require("./_partial/background");
 module.exports = class extends Component {
 	render() {
 		const { config, page, theme, partial } = this.props;
-
 		const language = page.lang || page.language || config.language;
+		const layout = "nexmoe";
+		const bodyClass = layout === "nexmoe" ? "mdui-drawer-body-left" : "";
+		const Content = require(`./_layout/${layout}/content`);
+		const Footer = require(`./_layout/${layout}/footer`);
 
 		return (
 			<html lang={language ? language.substr(0, 2) : ""}>
@@ -14,28 +17,33 @@ module.exports = class extends Component {
 						__html: partial("_partial/head"),
 					}}
 				></head>
-				<body class="mdui-drawer-body-left">
+				<body class={layout + " " + bodyClass}>
 					<Background {...this.props} />
 					<div
 						id="nexmoe-header"
 						dangerouslySetInnerHTML={{
-							__html: partial("_layout/single/header"),
+							__html: partial("_layout/nexmoe/header"),
 						}}
 					></div>
-					<div
-						id="nexmoe-content"
-						dangerouslySetInnerHTML={{
-							__html:
-								partial("_layout/single/content") +
-								partial("_partial/right"),
-						}}
-					></div>
+					<div id="nexmoe-content">
+						<Content {...this.props} />
+						<div
+							class="nexmoe-post-right"
+							dangerouslySetInnerHTML={{
+								__html: partial("_partial/right"),
+							}}
+						></div>
+					</div>
 
 					<div
 						dangerouslySetInnerHTML={{
 							__html: partial("_partial/searchbox"),
 						}}
 					></div>
+
+					<div id="nexmoe-footer">
+						<Footer {...this.props} />
+					</div>
 
 					<div
 						dangerouslySetInnerHTML={{
