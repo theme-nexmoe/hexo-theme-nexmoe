ec07ca28d4e775149af786a68e5883e9da3fdec6
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2023-03-19 12:53:55 +0800

    chore: paginator

--- layout/index.jsx
+++ layout/index.jsx
@@ -1,12 +1,16 @@
 const { Component } = require("inferno");
+const Paginator = require("./_partial/paginator");
 
 module.exports = class extends Component {
 	render() {
-		const { partial } = this.props;
+		const { page, partial } = this.props;
 		return (
 			<>
-				<section class="nexmoe-posts" dangerouslySetInnerHTML={{ __html: partial("_index/list") }}></section>
-                <div dangerouslySetInnerHTML={{ __html: partial("_partial/paginator") }}></div>
+				<section
+					class="nexmoe-posts"
+					dangerouslySetInnerHTML={{ __html: partial("_index/list") }}
+				></section>
+				{page.total > 1 ? <Paginator {...this.props} /> : ""}
 			</>
 		);
 	}
